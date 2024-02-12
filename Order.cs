b1063e6cdfacdf0681e196b7e0143257c32b5b95
Sokmontrey <sythatsokmontrey@gmail.com>
2024-02-12 18:27:24 -0500

    move: Edit and Summary to Order

--- Order.cs
+++ Order.cs
@@ -12,27 +12,191 @@ public struct Item {
 }
 
 /// <summary>
+/// Enum codes for after handle user input.
+/// </summary>
+public enum ModeCode {
+  NOTHING,
+  CONTINUE,
+  EDIT,
+  CURSOR_UP,
+  CURSOR_DOWN,
+  INCREMENT_ITEM,
+  DECREMENT_ITEM,
+  APPLY_COUPON,
+}
+
+/// <summary>
 /// Represents an order.
 /// </summary>
 public class Order {
-  private Item[] _items = new Item[0];
-  private float _subtotal = 0;
-  private float _tax_rate = 0;
-  private float _tax = 0;
-  private float _discount = 0;
-  private string _coupon = "";
-  private float _total = 0;
+  public Item[] items = new Item[0];
+  public float subtotal = 0;
+  public float tax_rate = 0;
+  public float tax = 0;
+  public float discount = 0;
+  public string coupon = "";
+  public float total = 0;
 
-  private int _cursor_pos;
+  public Dictionary<string, float> available_coupons;
 
-  public Order(Item[] items) { _items = items; }
+  public int cursor_pos;
 
-  public Order(Dictionary<string, float> menu) {
-    _items = new Item[menu.Count];
+  public Order(Dictionary<string, float> menu, float tax_rate, Dictionary<string, float> coupons) {
+    items = new Item[menu.Count];
     int i = 0;
     foreach (var item in menu) {
-      _items[i++] =
+      items[i++] =
           new Item { name = item.Key, price = item.Value, quantity = 0 };
+    }
+
+    this.tax_rate = tax_rate;
+    available_coupons = coupons;
+  }
+
+  public void Edit() {
+    while (true) {
+      Console.Clear();
+      DisplayManager.DisplayTitle();
+      DisplayManager.DisplayMenu(items, cursor_pos);
+      CalculateSubtotal();
+      DisplayManager.DisplaySubtotal(subtotal);
+      DisplayManager.DisplayEditInstructions();
+
+      ModeCode mode = GetEditInput();
+      switch (mode) {
+      case ModeCode.NOTHING:
+        continue;
+      case ModeCode.CONTINUE:
+        return;
+      case ModeCode.INCREMENT_ITEM:
+        AddItem();
+        break;
+      case ModeCode.DECREMENT_ITEM:
+        RemoveItem();
+        break;
+      case ModeCode.CURSOR_UP:
+        MoveUp();
+        break;
+      case ModeCode.CURSOR_DOWN:
+        MoveDown();
+        break;
+      }
+    }
+  }
+
+  private ModeCode GetEditInput() {
+    ConsoleKeyInfo key = Console.ReadKey(true);
+    switch (key.Key) {
+    case ConsoleKey.UpArrow:
+    case ConsoleKey.K:
+      return ModeCode.CURSOR_UP;
+    case ConsoleKey.DownArrow:
+    case ConsoleKey.J:
+      return ModeCode.CURSOR_DOWN;
+
+    case ConsoleKey.LeftArrow:
+    case ConsoleKey.H:
+      return ModeCode.DECREMENT_ITEM;
+    case ConsoleKey.RightArrow:
+    case ConsoleKey.L:
+      return ModeCode.INCREMENT_ITEM;
+
+    case ConsoleKey.Q:
+      Environment.Exit(0);
+      break;
+    case ConsoleKey.Enter:
+      return ModeCode.CONTINUE;
+    }
+    return ModeCode.NOTHING;
+  }
+
+  public bool Summary() {
+    while (true) {
+      CalculateTax(tax_rate);
+      CalculateTotal();
+      DisplayManager.DisplayOrderSummary(this);
+      DisplayManager.DisplaySummaryInstructions();
+
+      ModeCode mode = GetSummaryInput();
+      switch (mode) {
+      case ModeCode.EDIT:
+        return false;
+      case ModeCode.CONTINUE:
+        return true;
+      case ModeCode.APPLY_COUPON:
+        Console.Write("Enter coupon code: ");
+        ApplyCoupon(Console.ReadLine(), available_coupons);
+        break;
+      }
+    }
+  }
+
+  private ModeCode GetSummaryInput() {
+    ConsoleKeyInfo key = Console.ReadKey(true);
+    switch (key.Key) {
+    case ConsoleKey.Q:
+      Environment.Exit(0);
+      break;
+    case ConsoleKey.E:
+      return ModeCode.EDIT;
+    case ConsoleKey.Enter:
+      return ModeCode.CONTINUE;
+    case ConsoleKey.C:
+      return ModeCode.APPLY_COUPON;
+    }
+    return ModeCode.NOTHING;
+  }
+
+  public void MoveDown() { cursor_pos = (cursor_pos + 1) % items.Length; }
+
+  public void MoveUp() {
+    cursor_pos = (cursor_pos - 1 + items.Length) % items.Length;
+  }
+
+  public void AddItem() { items[cursor_pos].quantity++; }
+
+  public void RemoveItem() {
+    if (items[cursor_pos].quantity > 0) {
+      items[cursor_pos].quantity--;
+    }
+  }
+
+  public void ApplyCoupon(string code, Dictionary<string, float> coupons) {
+    coupon = code;
+    discount = 0;
+    if (coupons.ContainsKey(code))
+      discount = subtotal * coupons[code];
+  }
+
+  public float CalculateSubtotal() {
+    subtotal = 0;
+    for (int i = 0; i < items.Length; i++)
+      subtotal += items[i].price * items[i].quantity;
+    return subtotal;
+  }
+
+  public void CalculateTax(float tax_rate) {
+    this.tax_rate = tax_rate;
+    tax = subtotal * tax_rate;
+  }
+
+  public void CalculateTotal() { total = subtotal + tax - discount; }
+
+  public void HandleSaveOrder() {
+    Console.Write("Save to file? y/yes or press any key to exit: ");
+    string input = Console.ReadLine().ToLower();
+    if (input == "y" || input == "yes") {
+      Console.Write("Enter file name: ");
+      string file_name = Console.ReadLine();
+      while (file_name == "") {
+        Console.Write("File name can't be empty. Enter another file name: ");
+        file_name = Console.ReadLine();
+      }
+      while (File.Exists(file_name + ".txt")) {
+        Console.Write("File already exists. Enter another file name: ");
+        file_name = Console.ReadLine();
+      }
+      SaveToFile(file_name);
     }
   }
 
@@ -41,129 +205,23 @@ public class Order {
       writer.WriteLine("Your Order: \n");
       writer.WriteLine($"{"Item",-20}{"Price",-10}{"Qty",-10}");
       writer.WriteLine(new string('-', 40));
-      for (int i = 0; i < _items.Length; i++) {
-        if (_items[i].quantity <= 0)
+      for (int i = 0; i < items.Length; i++) {
+        if (items[i].quantity <= 0)
           continue;
 
         writer.WriteLine(
-            $"{_items[i].name,-20}{_items[i].price,-10:C2}{_items[i].quantity,-10}");
+            $"{items[i].name,-20}{items[i].price,-10:C2}{items[i].quantity,-10}");
       }
       writer.WriteLine(new string('-', 40));
-      writer.WriteLine($"{"Subtotal:",-20}{_subtotal:C2}");
-      writer.WriteLine($"{"Tax:",-20}{_tax:C2} ({_tax_rate:P0})");
+      writer.WriteLine($"{"Subtotal:",-20}{subtotal:C2}");
+      writer.WriteLine($"{"Tax:",-20}{tax:C2} ({tax_rate:P0})");
       writer.WriteLine(
-          $"{"Discount:",-20}{_discount:C2} (Coupon Code: {_coupon})");
+          $"{"Discount:",-20}{discount:C2} (Coupon Code: {coupon})");
       writer.WriteLine(new string('-', 40));
-      writer.WriteLine($"{"Total:",-20}{_total:C2}");
+      writer.WriteLine($"{"Total:",-20}{total:C2}");
     }
 
     Console.WriteLine($"Order saved to {filename}.txt");
-  }
-
-  public void MoveDown() { _cursor_pos = (_cursor_pos + 1) % _items.Length; }
-
-  public void MoveUp() {
-    _cursor_pos = (_cursor_pos - 1 + _items.Length) % _items.Length;
-  }
-
-  public void AddItem() { _items[_cursor_pos].quantity++; }
-
-  public void RemoveItem() {
-    if (_items[_cursor_pos].quantity > 0) {
-      _items[_cursor_pos].quantity--;
-    }
-  }
-
-  public void ApplyCoupon(string code, Dictionary<string, float> coupons) {
-    _coupon = code;
-    _discount = 0;
-    if (coupons.ContainsKey(code))
-      _discount = _subtotal * coupons[code];
-  }
-
-  public float CalculateSubtotal() {
-    _subtotal = 0;
-    for (int i = 0; i < _items.Length; i++)
-      _subtotal += _items[i].price * _items[i].quantity;
-    return _subtotal;
-  }
-
-  public void CalculateTax(float tax_rate) {
-    _tax_rate = tax_rate;
-    _tax = _subtotal * tax_rate;
-  }
-
-
-  public void DisplayMenu() {
-    Console.WriteLine($"{"Item",-22}{"Price",-10}{"Qty",-10}");
-    PrintSeparateLine();
-
-    for (int i = 0; i < _items.Length; i++) {
-      if (i == _cursor_pos) {
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.Write("> ");
-      } else {
-        Console.Write("  ");
-      }
-
-      Console.WriteLine(
-          $"{_items[i].name,-20}{_items[i].price,-10:C2}{_items[i].quantity,-10}");
-      Console.ResetColor();
-    }
-  }
-
-  public void DisplayOrder() {
-    Console.WriteLine($"{"Item",-20}{"Price",-10}{"Qty",-10}");
-    PrintSeparateLine();
-    for (int i = 0; i < _items.Length; i++) {
-      if (_items[i].quantity <= 0)
-        continue;
-
-      Console.WriteLine(
-          $"{_items[i].name,-20}{_items[i].price,-10:C2}{_items[i].quantity,-10}");
-    }
-  }
-
-  public void DisplaySubtotal() {
-    Console.WriteLine($"{"Subtotal:",-20}{_subtotal:C2}");
-  }
-
-  public void DisplayTax() {
-    Console.WriteLine($"{"Tax:",-20}{_tax:C2} ({_tax_rate:P0})");
-  }
-
-  public void DisplayDiscount() {
-    Console.WriteLine(
-        $"{"Discount:",-20}{_discount:C2} (Coupon Code: {_coupon})");
-  }
-
-  public void DisplayTotal() {
-    _total = _subtotal + _tax - _discount;
-    Console.WriteLine($"{"Total:",-20}{_total:C2}");
-  }
-
-  public void DisplaySummary() {
-    Console.Clear();
-    Console.WriteLine("Your Order: \n");
-    DisplayOrder();
-
-    PrintSeparateLine();
-
-    DisplaySubtotal();
-    DisplayTax();
-    DisplayDiscount();
-
-    PrintSeparateLine();
-
-    Console.ForegroundColor = ConsoleColor.Blue;
-    DisplayTotal();
-    Console.ResetColor();
-  }
-
-  public static void PrintSeparateLine() {
-    Console.ForegroundColor = ConsoleColor.DarkGray;
-    Console.WriteLine(new string('-', 40));
-    Console.ResetColor();
   }
 }
 
--- Program.cs
+++ Program.cs
@@ -3,178 +3,48 @@
 namespace OrderSystem {
 
 /// <summary>
-/// Enum codes for after handle user input.
-/// </summary>
-public enum ModeCode { NOTHING, CONTINUE, EDIT }
-
-/// <summary>
 /// Main entry point of the program
 /// </summary>
 public class Program {
-  public static Dictionary<string, float> _menu =
-      new Dictionary<string, float> {
-        { "Burger", 5.99f },
-        { "Fries", 2.99f },
-        { "Drink", 1.99f },
-      };
-
-  public static float _tax_rate = 0.13f;
-
-  public static Dictionary<string, float> _coupons =
-      new Dictionary<string, float> {
-        { "10OFF", 0.10f },
-        { "20OFF", 0.20f },
-        { "30OFF", 0.30f },
-      };
 
   public static void Main(string[] args) {
-    Order order = new Order(_menu);
-    ModeCode mode;
+    Order order = new Order(
+        new Dictionary<string, float> {
+          // menu
+          { "Burger", 5.99f },
+          { "Fries", 2.99f },
+          { "Drink", 1.99f },
+        },
+        0.13f, // 13% tax
+        new Dictionary<string, float> {
+          // available coupons
+          { "10OFF", 0.10f },
+          { "20OFF", 0.20f },
+          { "30OFF", 0.30f },
+        });
 
     while (true) {
-      mode = Edit(order);
+      order.Edit();
 
       // user can't continue with an empty order
-      if (order.CalculateSubtotal() == 0) {
+      if (order.subtotal == 0) {
         Console.WriteLine("Order is empty. Please add items to the order.");
         Console.Write("Press any key to continue...");
         Console.ReadKey(true);
         continue;
       }
 
-      // user have the decision to edit the order
-      mode = Summary(order);
-      if (mode == ModeCode.EDIT)
-        continue;
-      if (mode == ModeCode.CONTINUE)
+      // user can decision to edit the order
+      if (order.Summary())
         break;
     }
 
     Console.Clear();
-    order.DisplaySummary();
+    order.CalculateTotal();
+    DisplayManager.DisplayOrderSummary(order);
     Console.WriteLine("\nOrder submitted!");
-    Console.WriteLine("Thank you for your order!\n");
-    HandleSaveOrder(order);
-  }
-
-  private static void HandleSaveOrder(Order order) {
-    Console.Write("Save to file? y/yes or press any key to exit: ");
-    string input = Console.ReadLine().ToLower();
-    if (input == "y" || input == "yes") {
-      Console.Write("Enter file name: ");
-      string file_name = Console.ReadLine();
-      while (file_name == "") {
-        Console.Write("File name can't be empty. Enter another file name: ");
-        file_name = Console.ReadLine();
-      }
-      while (File.Exists(file_name + ".txt")) {
-        Console.Write("File already exists. Enter another file name: ");
-        file_name = Console.ReadLine();
-      }
-      order.SaveToFile(file_name);
-    }
-  }
-
-  private static ModeCode Edit(Order order) {
-    while (true) {
-      Console.Clear();
-      DisplayTitle();
-      order.DisplayMenu();
-
-      Console.WriteLine("");
-      order.CalculateSubtotal();
-      order.DisplaySubtotal();
-
-      DisplayEditInstructions();
-      ModeCode mode = GetEditInput(order);
-      if (mode != ModeCode.NOTHING)
-        return mode;
-    }
-  } 
-
-  private static ModeCode Summary(Order order) {
-    while (true) {
-      order.CalculateTax(_tax_rate);
-      order.DisplaySummary();
-
-      Console.WriteLine("");
-      DisplaySummaryInstructions();
-
-      ConsoleKeyInfo key = Console.ReadKey(true);
-      switch (key.Key) {
-      case ConsoleKey.Q:
-        Environment.Exit(0);
-        break;
-
-      case ConsoleKey.E:
-        return ModeCode.EDIT;
-
-      case ConsoleKey.Enter:
-        return ModeCode.CONTINUE;
-
-      case ConsoleKey.C:
-        Console.Write("Enter coupon code: ");
-        order.ApplyCoupon(Console.ReadLine(), _coupons);
-        break;
-      }
-    }
-  }
-
-  private static void DisplayTitle() {
-    Console.Write("\nWelcome to the");
-
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.Write(" par la rivière ");
-    Console.ResetColor();
-
-    Console.WriteLine("digital ordering system!\n");
-  } 
-
-  private static void DisplayEditInstructions() {
-    Console.ForegroundColor = ConsoleColor.DarkGray;
-    Console.WriteLine("\n  ↓/j   (down)       ↑/k (up)" +
-                      "\n  →/l   (add)        ←/h (remove)" +
-                      "\n  enter (continue)     q (quit)\n");
-    Console.ResetColor();
-  }
-
-  private static void DisplaySummaryInstructions() {
-    Console.ForegroundColor = ConsoleColor.DarkGray;
-    Console.WriteLine("e (edit)             c (apply coupon)" +
-                      "\nenter (continue)     q (quit)\n");
-    Console.ResetColor();
-  }
-
-  private static ModeCode GetEditInput(Order order) {
-    ConsoleKeyInfo key = Console.ReadKey(true);
-    switch (key.Key) {
-    case ConsoleKey.DownArrow:
-    case ConsoleKey.J:
-      order.MoveDown();
-      break;
-
-    case ConsoleKey.UpArrow:
-    case ConsoleKey.K:
-      order.MoveUp();
-      break;
-
-    case ConsoleKey.RightArrow:
-    case ConsoleKey.L:
-      order.AddItem();
-      break;
-
-    case ConsoleKey.LeftArrow:
-    case ConsoleKey.H:
-      order.RemoveItem();
-      break;
-
-    case ConsoleKey.Q:
-      Environment.Exit(0);
-      break;
-    case ConsoleKey.Enter:
-      return ModeCode.CONTINUE;
-    }
-    return ModeCode.NOTHING;
+    Console.WriteLine("Thank you for your purchases!\n");
+    order.HandleSaveOrder();
   }
 
 } // class Program
